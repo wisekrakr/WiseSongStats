d611f04f18aa8a8f58f88fc7f9b8a5ce28fbff76
David <wisekrakr@gmail.com>
2020-01-28 02:43:23 +0100

    fixing postbuild script

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", function(req, res) {
+app.use(express.static("build"));
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(9000);
