d1909539419f9cc4afad65addab92ae6bed37498
David <wisekrakr@gmail.com>
2020-01-28 03:22:55 +0100

    Updates

---
+++ precache-manifest.266444e2e2235574f4b7560c67bf4b35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd68bbb8ed63130c07b35e5d08bf687",
+    "url": "/WiseSongStats/index.html"
+  },
+  {
+    "revision": "fd2301966f84c4e4a575",
+    "url": "/WiseSongStats/static/css/main.395c3587.chunk.css"
+  },
+  {
+    "revision": "5da0426076d2b9c80acc",
+    "url": "/WiseSongStats/static/js/2.1b58b138.chunk.js"
+  },
+  {
+    "revision": "215a0ce7332e68e19d23a2b0fc13b3ec",
+    "url": "/WiseSongStats/static/js/2.1b58b138.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd2301966f84c4e4a575",
+    "url": "/WiseSongStats/static/js/main.bdc85ee5.chunk.js"
+  },
+  {
+    "revision": "7b018c2418af6db55b10",
+    "url": "/WiseSongStats/static/js/runtime-main.c1f93d58.js"
+  }
+]);
